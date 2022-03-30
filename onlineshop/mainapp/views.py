b7824518d30847401440bd9d9c5f5752afa63682
Вячеслав Бородин <vikc003@mail.ru>
2022-03-30 12:15:17 +0500

    add mixins

--- onlineshop/mainapp/views.py
+++ onlineshop/mainapp/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 from .models import Notebook, Smartphone, Category
-
-def test_view(request):
-    categories = Category.objects.get_categories_for_burger()
-    return render(request, 'base.html', {'categories': categories})
+from .mixins import CategoryDetailMixin
 
 
-class ProductDetailView(DetailView):
+class BaseView(View):
 
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_burger()
+        return render(request, 'base.html', {'categories': categories})
+
+
+# def test_view(request):
+#     categories = Category.objects.get_categories_for_burger()
+#     return render(request, 'base.html', {'categories': categories})
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone
@@ -23,4 +31,12 @@ class ProductDetailView(DetailView):
     # model = Model
     context_object_name = 'product'
     template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class CategoryDetailView(CategoryDetailMixin, DetailView):
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
