b3953397007182e679a14e916af92b092e65bef5
Вячеслав Бородин <vikc003@mail.ru>
2022-03-30 10:51:54 +0500

    add category names in burger

--- onlineshop/mainapp/models.py
+++ onlineshop/mainapp/models.py
@@ -10,10 +10,14 @@ import sys
 
 User = get_user_model()
 
+
+def get_models_for_count(*models_name):
+    return [models.Count(model_name) for model_name in models_name]
+
+
 def get_product_url(obj, viewname):
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
-
 
 
 class MinResolutionErrorExcepction(Exception):
@@ -48,9 +52,26 @@ class LatestProducts:
     objects = LatestProductsManager()
 
 
+class CategoryManager(models.Manager):
+
+    CATEGORY_NAME_COUNT_NAME = {
+        'Ноутбуки': 'notebook__count',
+        'Смартфоны': 'smartphone__count'
+    }
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_categories_for_burger(self):
+        models = get_models_for_count('notebook', 'smartphone')
+        qs = list(self.get_queryset().annotate(*models).values())
+        return [dict(name=c['name'], slug=c['slug'], count=c[self.CATEGORY_NAME_COUNT_NAME[c['name']]]) for c in qs]
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя категории')
     slug = models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
@@ -133,7 +154,6 @@ class Notebook(Product):
     def __str__(self):
         return f'{self.category.name, self.title}'
 
-
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
 
@@ -156,4 +176,3 @@ class Smartphone(Product):
 
     def get_absolute_url(self):
         return get_product_url(self, 'product_detail')
-
--- onlineshop/mainapp/views.py
+++ onlineshop/mainapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Notebook, Smartphone
+from .models import Notebook, Smartphone, Category
 
 def test_view(request):
-    return render(request, 'base.html', {})
+    categories = Category.objects.get_categories_for_burger()
+    return render(request, 'base.html', {'categories': categories})
 
 
 class ProductDetailView(DetailView):
