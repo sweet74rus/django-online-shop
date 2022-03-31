e67efcf2ba6ef41d27e977118818da90e63be58f
Вячеслав Бородин <vikc003@mail.ru>
2022-03-31 09:56:10 +0500

    add mixins cart

--- onlineshop/mainapp/mixins.py
+++ onlineshop/mainapp/mixins.py
@@ -1,5 +1,6 @@
 from django.views.generic.detail import SingleObjectMixin
-from .models import Category
+from .models import Category, Cart, Customer
+from django.views.generic import View
 
 class CategoryDetailMixin(SingleObjectMixin):
 
@@ -7,3 +8,20 @@ class CategoryDetailMixin(SingleObjectMixin):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.get_categories_for_burger()
         return context
+
+class CartMixin(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            customer = Customer.objects.filter(user=request.user).first()
+            if not customer:
+                customer = Customer.objects.create()
+            cart = Cart.objects.filter(owner=customer, in_order=False).first()
+            if not cart:
+                cart = Cart.objects.create(owner=customer)
+        else:
+            cart = Cart.object.filter(for_anonymous_user=True).first()
+            if not cart:
+                cart = Cart.objects.create(for_anonymous_user=True)
+
+        self.cart = cart
+        return super().dispatch(request, *args, **kwargs)
--- onlineshop/mainapp/views.py
+++ onlineshop/mainapp/views.py
@@ -3,24 +3,25 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
-from .mixins import CategoryDetailMixin
+from .mixins import CategoryDetailMixin, CartMixin
 
 
-class BaseView(View):
+class BaseView(CartMixin, View):
 
     def get(self, request, *args, **kwargs):
-        cart = Cart.objects.first()
+        #cart = Cart.objects.first()
+
         categories = Category.objects.get_categories_for_burger()
         products = LatestProducts.objects.get_products_for_main_page('notebook', 'smartphone')
         context = {
             'categories': categories,
             'products': products,
-            'cart': cart
+            'cart': self.cart
         }
         return render(request, 'base.html', context)
 
 
-class ProductDetailView(CategoryDetailMixin, DetailView):
+class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone
@@ -40,7 +41,7 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
         context['ct_model'] = self.model._meta.model_name
         return context
 
-class CategoryDetailView(CategoryDetailMixin, DetailView):
+class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
@@ -48,27 +49,28 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     slug_url_kwarg = 'slug'
 
 
-class AddToCartView(View):
+class AddToCartView(CartMixin, View):
 
     def get(self, request, *args, **kwargs):
         ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
-        cart = Cart.objects.first()
+        #cart = Cart.objects.first()
         content_type = ContentType.objects.get(model=ct_model)
         product = content_type.model_class().objects.get(slug=product_slug)
         cart_product, created = CartProduct.objects.get_or_create(
-            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
         )
-        cart.products.add(cart_product)
+        if created:
+            self.cart.products.add(cart_product)
         return HttpResponseRedirect('/cart/')
 
 
-class CartView(View):
+class CartView(CartMixin, View):
 
     def get(self, request, *args, **kwargs):
-        cart = Cart.objects.first()
+        #cart = Cart.objects.first()
         categories = Category.objects.get_categories_for_burger()
         context = {
-            'cart': cart,
+            'cart': self.cart,
             'categories': categories
         }
         return render(request, 'cart.html', context)
