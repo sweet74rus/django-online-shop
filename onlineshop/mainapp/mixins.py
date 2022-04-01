e0c468d687464510986c674d5cf749fef875b529
Вячеслав Бородин <vikc003@mail.ru>
2022-04-01 09:04:06 +0500

    add categories products

--- onlineshop/mainapp/mixins.py
+++ onlineshop/mainapp/mixins.py
@@ -1,10 +1,21 @@
 from django.views.generic.detail import SingleObjectMixin
-from .models import Category, Cart, Customer
+from .models import Category, Cart, Customer, Notebook, Smartphone
 from django.views.generic import View
 
 class CategoryDetailMixin(SingleObjectMixin):
 
+    CATEGORY_SLUG2PRODUCT_MODEL = {
+        'notebooks': Notebook,
+        'smartphones': Smartphone
+    }
+
     def get_context_data(self, **kwargs):
+        if isinstance(self.get_object(), Category):
+            model = self.CATEGORY_SLUG2PRODUCT_MODEL[self.get_object().slug]
+            context = super().get_context_data(**kwargs)
+            context['categories'] = Category.objects.get_categories_for_burger()
+            context['category_products'] = model.objects.all()
+            return context
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.get_categories_for_burger()
         return context
